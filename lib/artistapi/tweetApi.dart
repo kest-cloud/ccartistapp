5ae72b2e753c08beca5495be0ff78362d537a2e4
kest-cloud <afolanet.co.uk@gmail.com>
2021-09-02 14:20:22 +0100

    worked on the Apis, converted it from Json, made it ready for use|

--- lib/artistapi/tweetApi.dart
+++ lib/artistapi/tweetApi.dart
@@ -1,39 +1,27 @@
 import 'dart:convert';
-import 'dart:core';
-import 'package:ccartistapp/models/artistdetailsmodel.dart';
-import 'package:ccartistapp/models/postmodel.dart';
 import 'package:http/http.dart' as http;
+import 'package:ccartistapp/models/postmodel.dart';
 
-class PostApi {
+class TweetApi {
   String url = "https://jsonplaceholder.typicode.com";
 
   Future<List<Post>> findAll() async {
-    var response = await http.get(Uri.parse('$url/users'));
+    var response = await http.get(Uri.parse('$url/comments'));
+
     if (response.statusCode == 200) {
       List<dynamic> body = jsonDecode(response.body);
 
-      return body.map((users) => Post.fromJson(users)).toList();
+      return body.map((posts) => Post.fromJson(posts)).toList();
       // return body.fromJson(jsonDecode(response.body));
       //return body.map((posts) => Post.fromJson(posts));
     } else {
-      throw Exception("Fail to list all artist");
+      throw Exception("Fail to create a Post");
     }
   }
 
-  Future<Post> find(int id) async {
-    // ignore: unnecessary_brace_in_string_interps
-    var response = await http.get(Uri.parse('$url/albums/${id}/photos'));
-
-    if (response.statusCode == 200) {
-      return Post.fromJson(jsonDecode(response.body));
-    } else {
-      throw Exception("Fail load albums");
-    }
-  }
-
-  Future createTweet(Post post) async {
+  Future updateTweet(Post post) async {
     final response = await http.post(
-      Uri.parse('$url/comments'),
+      Uri.parse('$url/comments/${post.id!}'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
@@ -46,7 +34,23 @@ class PostApi {
       // If the server did not return a 201 CREATED response,
       // then throw an exception.
       print(response.statusCode);
-      throw Exception('Failed to create new Tweet.');
+      throw Exception('Failed to update tweet.');
+    }
+  }
+
+  Future deleteTweet(String id) async {
+    final http.Response response = await http.delete(
+      Uri.parse('$url/comments/$id'),
+      headers: <String, String>{
+        'Content-Type': 'application/json; charset=UTF-8',
+      },
+    );
+
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      // then throw an exception.
+      throw Exception('Failed to Delete Tweet.');
     }
   }
 }
